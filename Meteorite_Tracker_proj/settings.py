1d5b8771664cb5d4bfb58ba2e0fb963071a34ffc
Tyler Giles <tylerjaygiles@gmail.com>
2022-06-15 19:23:13 -0700

    added models and app

--- Meteorite_Tracker_proj/settings.py
+++ Meteorite_Tracker_proj/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
 
     'users_app',
+    'cosmic_event_app',
 ]
 
 MIDDLEWARE = [
--- cosmic_event_app/models.py
+++ cosmic_event_app/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from users_app import CustomUser
+from users_app.models import CustomUser
 
 # Create your models here.
 
 
 class CosmicEvent(models.Model):
-    mass = models.DecimalField(max_digits=None, decimal_places=4)
+    mass = models.DecimalField(max_digits=50, decimal_places=4)
     found = models.BooleanField()
     year = models.DateTimeField()
-    latitude = models.DecimalField(max_digits=None, decimal_places=6)
-    longitude = models.DecimalField(max_digits=None, decimal_places=6)
-    user = models.ForeignKey(CustomUser)
+    latitude = models.DecimalField(max_digits=50, decimal_places=6)
+    longitude = models.DecimalField(max_digits=50, decimal_places=6)
+    user = models.ForeignKey(CustomUser, null=True, on_delete=models.SET_NULL)
