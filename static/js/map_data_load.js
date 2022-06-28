a62bf687fa4965466d89348befee8ba7504fcc9d
Tyler Giles <tylerjaygiles@gmail.com>
2022-06-27 18:16:30 -0700

    working map and markers bound to user events

--- static/js/map_data_load.js
+++ static/js/map_data_load.js
@@ -1,21 +1,34 @@
-console.log('axios started')
-// const axios = require('axios');
-const token = document.getElementsByName("csrfmiddlewaretoken")[0]
+// Lat first then longitude
 
+const token = document.getElementsByName("csrfmiddlewaretoken")[0];
 
+var logVar = "";
+var latVar = "";
+
+const addMarker = () => {
+  L.marker([parseInt(latVar), parseInt(logVar)])
+    .addTo(map)
+    .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+    .openPopup();
+};
 
 axios({
-    headers: {
-        "Content-Type": "application/json",
-        "X-CRFToken": token,
-    },
-    xsrfHeaderName: "X-CRFToken",
-    method: 'GET',
-    url: '/event_retrieve',
-    
-}).then(function (response){
-    console.log(response.data);
-}); 
-
-
-
+  headers: {
+    "Content-Type": "application/json",
+    "X-CRFToken": token,
+  },
+  xsrfHeaderName: "X-CRFToken",
+  method: "GET",
+  url: "/event_retrieve",
+}).then(function (response) {
+    console.log(response.data)
+  for (let i = 0; i < response.data.length; i++) {
+    console.log(response.data.length)
+    logVar = response.data[i].longitude;
+    latVar = response.data[i].latitude;
+    addMarker();
+  }
+});
+//   .then(function (results) {
+//     addMarker();
+//   });
